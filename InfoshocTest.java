c1f6f8569c7891ab66ceb1ad257d48a985ad65e3
infoshoc <infoshoc@infoshoc.infoshoc>
2018-12-19 21:28:15 +0200

    testing limit 10 in getTopCountryPlaylists

--- InfoshocTest.java
+++ InfoshocTest.java
@@ -1108,10 +1108,30 @@ public class InfoshocTest extends AbstractTest {
         Song song1 = createSong(1, "I", "Alt", "USA", 0);
         Song song2 = createSong(2, "I", "Alt", "UK", 0);
         Song song3 = createSong(3, "I", "Alt", "USA", 0);
+        Song song4 = createSong(4, "I am the mountain", "Stoner", null, 0);
+        Song song5 = createSong(5, "I", "Alt", "USA", 0);
+        Song song6 = createSong(6, "I", "Alt", null, 0);
+        Song song7 = createSong(7, "I", "Alt", "USA", 0);
+        Song song8 = createSong(8, "I", "Alt", "USA", 0);
+        Song song9 = createSong(9, "I", "Alt", "USA", 0);
+        Song song10 = createSong(10, "I", "Alt", "USA", 0);
+        Song song11 = createSong(11, "I", "Alt", "USA", 0);
+        Song song12 = createSong(12, "I", "Alt", "USA", 0);
+        Song song13 = createSong(13, "I", "Alt", "USA", 0);
+
         Playlist playlist1 = createPlayList(1, "Alt", "");
         Playlist playlist2 = createPlayList(2, "Alt", "");
         Playlist playlist3 = createPlayList(3, "Alt", "");
         Playlist playlist4 = createPlayList(4, "Alt", "");
+        Playlist playlist5 = createPlayList(5, "Stoner", "Stoned Jesus");
+        Playlist playlist6 = createPlayList(6, "Alt", "");
+        Playlist playlist7 = createPlayList(7, "Alt", "");
+        Playlist playlist8 = createPlayList(8, "Alt", "");
+        Playlist playlist9 = createPlayList(9, "Alt", "");
+        Playlist playlist10 = createPlayList(10, "Alt", "");
+        Playlist playlist11 = createPlayList(11, "Alt", "");
+        Playlist playlist12 = createPlayList(12, "Alt", "");
+        Playlist playlist13 = createPlayList(13, "Alt", "");
 
         assertEquals(new ArrayList<Integer>(), Solution.getTopCountryPlaylists(null));
         assertEquals(new ArrayList<Integer>(), Solution.getTopCountryPlaylists(-1));
@@ -1123,10 +1143,29 @@ public class InfoshocTest extends AbstractTest {
         assertOK(Solution.addSong(song1));
         assertOK(Solution.addSong(song2));
         assertOK(Solution.addSong(song3));
+        assertOK(Solution.addSong(song4));
+        assertOK(Solution.addSong(song5));
+        assertOK(Solution.addSong(song6));
+        assertOK(Solution.addSong(song7));
+        assertOK(Solution.addSong(song8));
+        assertOK(Solution.addSong(song9));
+        assertOK(Solution.addSong(song10));
+        assertOK(Solution.addSong(song11));
+        assertOK(Solution.addSong(song12));
+        assertOK(Solution.addSong(song13));
         assertOK(Solution.addPlaylist(playlist1));
         assertOK(Solution.addPlaylist(playlist2));
         assertOK(Solution.addPlaylist(playlist3));
         assertOK(Solution.addPlaylist(playlist4));
+        assertOK(Solution.addPlaylist(playlist5));
+        assertOK(Solution.addPlaylist(playlist6));
+        assertOK(Solution.addPlaylist(playlist7));
+        assertOK(Solution.addPlaylist(playlist8));
+        assertOK(Solution.addPlaylist(playlist9));
+        assertOK(Solution.addPlaylist(playlist10));
+        assertOK(Solution.addPlaylist(playlist11));
+        assertOK(Solution.addPlaylist(playlist12));
+        assertOK(Solution.addPlaylist(playlist13));
         assertOK(Solution.addSongToPlaylist(2, 1));
         assertOK(Solution.addSongToPlaylist(1, 3));
         assertOK(Solution.addSongToPlaylist(3, 3));
@@ -1141,6 +1180,8 @@ public class InfoshocTest extends AbstractTest {
         assertOK(Solution.songPlay(2, 42));
         assertOK(Solution.songPlay(1, 23));
         assertOK(Solution.songPlay(3, 7));
+        assertOK(Solution.songPlay(4, 69));
+        assertOK(Solution.addSongToPlaylist(4, 5));
 
 
         assertEquals(new ArrayList<Integer>(), Solution.getTopCountryPlaylists(null));
@@ -1155,7 +1196,209 @@ public class InfoshocTest extends AbstractTest {
             add(3);
         }}, Solution.getTopCountryPlaylists(2));
 
-        // TODO: Test limit
+        assertOK(Solution.addSongToPlaylist(5, 7));
+
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.songPlay(5, 26));
+
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.addSongToPlaylist(6, 6));
+
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.songPlay(6, 26));
+
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.addSongToPlaylist(7, 6));
+
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(6);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.songPlay(7, 1));
+
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(6);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.addSongToPlaylist(7, 10));
+
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(6);
+            add(7);
+            add(10);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.addSongToPlaylist(8, 10));
+
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(6);
+            add(7);
+            add(10);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.songPlay(8, 27));
+        assertOK(Solution.addSongToPlaylist(9, 9));
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(10);
+            add(6);
+            add(7);
+            add(9);
+        }}, Solution.getTopCountryPlaylists(2));
+        assertOK(Solution.songPlay(9, 28));
+
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(9);
+            add(10);
+            add(6);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.addSongToPlaylist(10, 8));
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(9);
+            add(10);
+            add(6);
+            add(7);
+            add(8);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.songPlay(10, 28));
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(8);
+            add(9);
+            add(10);
+            add(6);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.addSongToPlaylist(11, 12));
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(8);
+            add(9);
+            add(10);
+            add(6);
+            add(7);
+            add(12);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.songPlay(11, 29));
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(12);
+            add(8);
+            add(9);
+            add(10);
+            add(6);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+
+        assertOK(Solution.addSongToPlaylist(12, 11));
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(12);
+            add(8);
+            add(9);
+            add(10);
+            add(6);
+            add(7);
+            add(11);
+        }}, Solution.getTopCountryPlaylists(2));
+        assertOK(Solution.songPlay(12, 29));
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(11);
+            add(12);
+            add(8);
+            add(9);
+            add(10);
+            add(6);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+        assertOK(Solution.addSongToPlaylist(13, 13));
+        assertEquals(new ArrayList<Integer>(){{
+            add(2);
+            add(4);
+            add(3);
+            add(11);
+            add(12);
+            add(8);
+            add(9);
+            add(10);
+            add(6);
+            add(7);
+        }}, Solution.getTopCountryPlaylists(2));
+        assertOK(Solution.songPlay(13, 321));
+        assertEquals(new ArrayList<Integer>(){{
+            add(13);
+            add(2);
+            add(4);
+            add(3);
+            add(11);
+            add(12);
+            add(8);
+            add(9);
+            add(10);
+            add(6);
+        }}, Solution.getTopCountryPlaylists(2));
     }
 
     @Test
